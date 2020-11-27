fd5da7f94e830d021dc425fdba081c6936517fe8
Thomas Winter <wintert98@gmail.com>
2020-11-27 15:34:40 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,6 +15,8 @@ def hopper
         :languages => ["C"]
       }
     }
+    
+programmer_hash[:grace_hopper]
 
 end
 
@@ -36,6 +38,8 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,7 +59,9 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+    
+    programmer_hash[:dennis_ritchie][:languages][0]
+    
 end
 
 def adding_matz
@@ -81,6 +87,8 @@ def adding_matz
         :languages => ["C"]
       }
     }
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+programmer_hash
 
 end
 
@@ -103,7 +111,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for] = "GUI"
+programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +134,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 end
