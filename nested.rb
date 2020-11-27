0c67ef668ab86fbc3ff01de20ac2a721fdf41227
Thomas Winter <wintert98@gmail.com>
2020-11-27 15:22:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,8 +87,8 @@ def adding_matz
         :languages => ["C"]
       }
     }
-programmer_hash[:yukihiro_matsumoto ][:known_for] = "Ruby"
-programmer_hash[:yukihiro_matsumoto ][:languages] = ["LISP", "C"]
+programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 programmer_hash
 end
 
